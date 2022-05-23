6b0567d115ad72bbefcd93ea2e32d8001fd92604
deequoique <puppy_road@163.com>
2022-05-23 16:09:43 +0800

    idinput

--- app/src/main/java/com/example/androidaircraft/activity/InputActivity.java
+++ app/src/main/java/com/example/androidaircraft/activity/InputActivity.java
@@ -1,6 +1,23 @@
 package com.example.androidaircraft.activity;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.androidaircraft.R;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class InputActivity extends AppCompatActivity {
+//    private TextInputEditText input;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.id_input);
+//        input = new TextInputEditText(this);
+    }
+//    public void Confirm(View view) {
+//        String c = input.getText().toString();
+//    }
 }
